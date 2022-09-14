4a85538bf74d7dc660c1eba609ef198be3ad6175
Mojtaba Motevali <m.motevali1376@gmail.com>
2022-09-14 19:43:17 +0430

    added unit tests

--- entry-point.sh
+++ entry-point.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+if [ $STAGE == "test"]
+then
+    yarn run test:int 
+fi
+
 MONGO_SERVICE="$DB_HOST:$DB_PORT"
 echo "Wait for MONGODB=${MONGO_SERVICE}"
 
 WAIT4PORTS_TIMEOUT=20 wait4ports -q  tcp://$MONGO_SERVICE
 
-if [ $STAGE == "integrationTest" ]
+if [ $STAGE == "test" ]
 then
     yarn run test:int
 else
--- test.Dockerfile
+++ test.Dockerfile
@@ -1,9 +1,9 @@
-FROM node:14.17.0-alpine AS test-build
+FROM node:14.17.0-alpine AS test-base
 WORKDIR /app
 RUN apk add bash wait4ports
 COPY package.json .
 COPY yarn.lock .
-ENV STAGE=integrationTest
+ENV STAGE=test
 RUN yarn install
 COPY . .
 ENTRYPOINT [ "bash", "entry-point.sh" ]
